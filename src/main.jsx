5dee938a9fceb614f7b72bab3dc47ba77b8ba10b
ProudOne7 <peternyen7@gmail.com>
2023-12-23 16:01:18 -0600

    Added Symbols To Midsection

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react"
 import cat1 from "/images/staringOrangeCat.png"
 import cat2 from "/images/violentOrangeCat.jpg"
 import cat3 from "/images/wackyOrangeCat.jpg"
+import texasPic from "/images/texas.png"
+import airplane from "/images/airplane.png"
+import moneySymbol from "/images/moneySymbol.png"
 
 
 export default function Main(){
@@ -13,7 +16,14 @@ export default function Main(){
                     <p>Explore our varieties below</p>
                 </div>
             </div>
-            <div className = "mid-section"></div>
+            <div className = "mid-section">
+                <div className = "mid-section-imgs">
+                    <img src = {texasPic}/>
+                    <img src = {airplane}/>
+                    <img src = {moneySymbol}/>
+                </div>
+                <div className = "mid-section-caption"></div>
+            </div>
             <div className ="example-cats">
                     <img src = {cat1}/>
                     <img src = {cat2}/>
