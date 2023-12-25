2b276ae370bf26126fdcdedae805adfdde11b412
ProudOne7 <peternyen7@gmail.com>
2023-12-24 23:26:04 -0600

    Fixed Format for Mobile Devices

--- app.jsx
+++ app.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client"
 import Header from "./src/Header.jsx"
 import Main from "./src/Main.jsx"
 
+let siteWidth = 1280;
+let scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
 function App(){
     return(
