30c6aa4749267ea754bdad04a4b3582e6eead22c
ProudOne7 <peternyen7@gmail.com>
2023-12-29 13:42:04 -0600

    Adding page components

---
+++ pages/shoppingPage.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function ShoppingPage(){
+    return(
+    <div>
+        <div className = "shopping-page-header"></div>
+        <div className = "shopping-page-body">
+            <div className = "cat-cards">
+
+            </div>
+        </div>
+    </div>
+    )
+}
