f1a92c3f8178288e20d5131d34bb8a814f04359d
ProudOne7 <peternyen7@gmail.com>
2023-12-29 19:07:48 -0600

    Initial File with Cat Cards Info

---
+++ components/catCardInfo.jsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
+    }
+]
