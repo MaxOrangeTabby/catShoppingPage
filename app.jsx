a10e42ca9a10df845ac1fa6c271b74690b98b604
ProudOne7 <peternyen7@gmail.com>
2023-12-29 19:22:26 -0600

    Added Multiple Pages

--- app.jsx
+++ app.jsx
@@ -1,19 +1,24 @@
 import React from "react"
+import { BrowserRouter as Router,Routes, Route} from "react-router-dom"
 import ReactDOM from "react-dom/client"
-import Header from "./src/Header.jsx"
 import Main from "./src/Main.jsx"
+import ShoppingPage from "./pages/shoppingPage.jsx"
+
 
 let siteWidth = 1280;
 let scale = screen.width /siteWidth;
 
 document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
 
+
 function App(){
     return(
-        <div>
-            <Header/>
-            <Main />
-        </div>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/shopping-page" element={<ShoppingPage />} />
+            </Routes>
+    </Router>
     )
 }
 
