81d67ee982bcfeb759b7e8ddac9307529f0e1e01
ProudOne7 <peternyen7@gmail.com>
2023-12-29 19:07:25 -0600

    Added Title and Title Captain

--- pages/shoppingPage.jsx
+++ pages/shoppingPage.jsx
@@ -1,12 +1,36 @@
 import React from "react"
+import CatCard from "../components/catCard.jsx"
+import {Link} from "react-router-dom"
+import catCardInfo from "../components/catCardInfo.jsx"
 
 export default function ShoppingPage(){
+    const catCards = catCardInfo.map(function(card){
+        return(
+            <CatCard
+                img = {card.img}
+                color = {card.count}
+                count = {card.count}
+                price = {card.price}
+            
+            />
+        )
+    })
+
     return(
     <div>
-        <div className = "shopping-page-header"></div>
+        <div className = "shopping-page-header">
+            <Link to = "/">
+                <button className = "shopping-page-back-btn" type = "button">Back</button>
+            </Link>
+            <h1 className = "shopping-page-title">Shopping Page</h1>
+            <img className = "shopping-cart-logo" src = "./images/shoppingCart.png" />
+        </div>
+        <div className = "shopping-page-header-caption">
+            <p>5% OFF ALL ORDERS OVER $99,999</p>
+        </div>
         <div className = "shopping-page-body">
             <div className = "cat-cards">
-
+                {catCards}
             </div>
         </div>
     </div>
