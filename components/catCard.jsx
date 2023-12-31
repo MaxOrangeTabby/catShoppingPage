9778c31a199943e7c78ffca1dc0836ac173a04c0
ProudOne7 <peternyen7@gmail.com>
2023-12-30 20:08:02 -0600

    Added Info to Cat Card

--- components/catCard.jsx
+++ components/catCard.jsx
@@ -4,7 +4,12 @@ export default function CatCard(props){
     return(
         <div className = "cat-card">
             <img className = "cat-card-img" src = {props.img}></img>
-            <div className = "cat-card-info"></div>
+            <div className = "cat-card-info">
+                <p>Color: {props.color}</p>
+                <p>Quantity: {props.count}</p>
+                <p>Price: ${props.price}</p>
+            </div>
+            
         </div>
     )
 }
