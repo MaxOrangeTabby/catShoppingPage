292c82de867ffab0551ba13bd96977c58ba39962
ProudOne7 <peternyen7@gmail.com>
2023-12-30 20:07:40 -0600

    Fixed Bug with Prop Info

--- pages/shoppingPage.jsx
+++ pages/shoppingPage.jsx
@@ -8,7 +8,7 @@ export default function ShoppingPage(){
         return(
             <CatCard
                 img = {card.img}
-                color = {card.count}
+                color = {card.color}
                 count = {card.count}
                 price = {card.price}
             
