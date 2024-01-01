ed7f0f14e23bc75d514e7fa34f0bc7bbe9d34e5c
ProudOne7 <peternyen7@gmail.com>
2023-12-31 19:40:49 -0600

    Added Add To Cart Button

--- components/catCard.jsx
+++ components/catCard.jsx
@@ -3,13 +3,15 @@ import React from "react"
 export default function CatCard(props){
     return(
         <div className = "cat-card">
-            <img className = "cat-card-img" src = {props.img}></img>
-            <div className = "cat-card-info">
-                <p>Color: {props.color}</p>
-                <p>Quantity: {props.count}</p>
-                <p>Price: ${props.price}</p>
+            <div className = "cat-card-top">
+                <img className = "cat-card-img" src = {props.img}></img>
+                <div className = "cat-card-info">
+                    <p>Color: {props.color}</p>
+                    <p>Quantity: {props.count}</p>
+                    <p>Price: ${props.price}</p>
+                </div>
             </div>
-            
+            <button className = "add-cart-btn">Add To Cart</button>
         </div>
     )
 }
