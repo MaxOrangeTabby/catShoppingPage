134423242a345c97a24bb6fd9b0a84b533024d45
ProudOne7 <peternyen7@gmail.com>
2024-01-08 18:43:33 -0600

    Added Logo and Text to Login Box

--- pages/loginPage.jsx
+++ pages/loginPage.jsx
@@ -4,6 +4,12 @@ export default function LoginPage(){
     return(
         <div className = "login-page">
             <div className = "login-box">
+                <div className = "login-header">
+                    <div className = "login-header-logo">
+                        <img src = "../images/logo.png"/>
+                    </div>
+                    <h1> Sign In To Account</h1>
+                </div>
                 <form className = "login-form">
                     <input type="text"/>
                     <input type="text"/>
