65c38cd535d20cc22c1e7e15b130b42b096b035e
ProudOne7 <peternyen7@gmail.com>
2024-01-11 20:27:17 -0600

    Added More Cat Selections

--- components/catCardInfo.jsx
+++ components/catCardInfo.jsx
@@ -94,6 +94,151 @@ export default [
         color: "Grey and White",
         count: -1,
         price: 9000,
+    },
+
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
+    },
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
+    },
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
+    },
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
+    },
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
+    },
+    {
+        id: 0,
+        img: "../images/catCard1.jpg",
+        color: "Brown and White",
+        count: 5,
+        price: 7777,
+
+    },
+
+    {
+        id: 1,
+        img: "../images/catCard2.jpg",
+        color: "Brown",
+        count: 100,
+        price: 2,
+    },
+
+    {
+        id: 2,
+        img: "../images/catCard3.jpg",
+        color: "Grey and White",
+        count: -1,
+        price: 9000,
     }
     
 
